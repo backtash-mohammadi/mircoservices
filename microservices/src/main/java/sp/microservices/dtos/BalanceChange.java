c75cb28ef0a33902ab10ba5affb24a0ff65f9338
Backtash Mohammadi <back20012@gmail.com>
2025-08-22 14:59:28 +0200

    changed to lombok and record and it works

--- microservices/src/main/java/sp/microservices/dtos/BalanceChange.java
+++ microservices/src/main/java/sp/microservices/dtos/BalanceChange.java
@@ -1,21 +1,17 @@
 package sp.microservices.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BalanceChange {
     private String username;
     private BigDecimal delta;
-
-    public BalanceChange() {}
-
-    public BalanceChange(String username, BigDecimal delta) {
-        this.username = username;
-        this.delta = delta;
-    }
-
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-    public BigDecimal getDelta() { return delta; }
-    public void setDelta(BigDecimal delta) { this.delta = delta; }
 }
+
 
--- microservices/src/main/java/sp/microservices/dtos/User.java
+++ microservices/src/main/java/sp/microservices/dtos/User.java
@@ -3,20 +3,9 @@ package sp.microservices.dtos;
 
 import java.math.BigDecimal;
 
-public class User {
-    private String username;
-    private BigDecimal balance;
-
-    public User() {}
-
-    public User(String username, BigDecimal balance) {
-        this.username = username;
-        this.balance = balance;
+public record User(String username, BigDecimal balance) {
+    public User {
+        if (balance == null) balance = BigDecimal.ZERO;
     }
-
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-    public BigDecimal getBalance() { return balance; }
-    public void setBalance(BigDecimal balance) { this.balance = balance; }
 }
 
